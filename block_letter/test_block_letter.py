6abc746d45b6b06a985f4b5e11966534fe9bf26a
Viet Hoang <1300077+vietqhoang@users.noreply.github.com>
2022-05-12 17:24:29 -0700

    Project: Block Letter (Revisions) (#3)
    Made some minor improvements.
    
    * Use of a `splat` instead of `list()` to convert a string into a list of characters.
    * Add descriptions to tests
    * Add a `main.py` to the project to execute code per the project's key result.

--- block_letter/test_block_letter.py
+++ block_letter/test_block_letter.py
@@ -6,6 +6,8 @@ class TestBlockLetter(unittest.TestCase):
     self.block_letter = BlockLetter
 
   def test_characters_with_v(self):
+    'When the argument is is an `V`, it is expected to output the `V` block letter'
+
     expectation = (
       'V   V\n'
       'V   V\n'
@@ -19,6 +21,8 @@ class TestBlockLetter(unittest.TestCase):
     self.assertEqual(self.block_letter.characters('V'), expectation)
 
   def test_characters_with_q(self):
+    'When the argument is is an `Q`, it is expected to output the `Q` block letter'
+
     expectation = (
       ' QQQ \n'
       'Q   Q\n'
@@ -32,6 +36,8 @@ class TestBlockLetter(unittest.TestCase):
     self.assertEqual(self.block_letter.characters('Q'), expectation)
 
   def test_characters_with_h(self):
+    'When the argument is is an `H`, it is expected to output the `H` block letter'
+
     expectation = (
       'H   H\n'
       'H   H\n'
@@ -45,6 +51,8 @@ class TestBlockLetter(unittest.TestCase):
     self.assertEqual(self.block_letter.characters('H'), expectation)
 
   def test_characters_with_vqh(self):
+    'When the argument is multiple characters, it is expected to output characters from left-to-right'
+
     expectation = (
       'V   V  QQQ  H   H\n'
       'V   V Q   Q H   H\n'
